446a1b72ba618d791b3b75e6efa354697def5d89
Godzshaka <yagokades@hotmail.com>
2019-11-10 20:00:34 -0300

    Atualização root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+   root to: 'repositories'
    post '/repositories/update_trending', to: 'repositories#update_trending'  
-  resources :repositories
-
+   resources :repositories
 end
